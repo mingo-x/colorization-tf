87b005657c869b96f80ab52f3ccd43c7db22110d
Yanping <rea.s.xyp@gmail.com>
2018-07-26 11:42:08 +0200

    Change train dir. Add printing.

--- tools/create_imagenet_list.py
+++ tools/create_imagenet_list.py
@@ -4,6 +4,7 @@ f = open('data/train.txt', 'w')
 imagenet_basepath = '/srv/glusterfs/xieya/data/imagenet1k_uncompressed/train/'
 for p1 in os.listdir(imagenet_basepath):
     if p1.startswith('n'):
+        print(p1)
         for p2 in os.listdir(imagenet_basepath + p1 + '/'):
             image = os.path.abspath(imagenet_basepath + p1 + '/' + p2)
             f.write(image + '\n')
