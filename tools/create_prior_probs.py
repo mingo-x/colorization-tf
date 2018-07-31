c0d2e863c895828913769c81d6e7c62562197549
Yanping <rea.s.xyp@gmail.com>
2018-07-31 14:52:46 +0200

    Test.

--- tools/create_prior_probs.py
+++ tools/create_prior_probs.py
@@ -4,11 +4,11 @@
 # ----- Parameters passed to the cluster -------
 ## <= 1h is short queue, <= 6h is middle queue, <= 48 h is long queue
 
-#$ -t 1:100
+#$ -t 1:2
 
 #$ -S /srv/glusterfs/xieya/anaconda2/bin/python
 
-#$ -l h_rt=5:59:59
+#$ -l h_rt=0:59:59
 
 #$ -l h_vmem=4G
 
@@ -35,65 +35,67 @@ _IMG_PATHS = '/home/xieya/colorization-tf/data/train.txt'
 _POINTS_PATH = '/home/xieya/colorization-tf/resources/pts_in_hull.npy'
 _PRINT_FREQ = 10
 _TASK_ID = int(os.environ.get('SGE_TASK_ID')) - 1
+print _TASK_ID
+exit()
 
 
-def _get_img_list():
-  img_list = []  
-  img_count = 0
-  with open(_IMG_PATHS, 'r') as fin:
-    for img_path in fin:
-      if img_count % _NUM_TASKS == _TASK_ID:
-        img_path = img_path.strip()
-        img_list.append(img_path)
-      img_count += 1
-  return img_list
+# def _get_img_list():
+#   img_list = []  
+#   img_count = 0
+#   with open(_IMG_PATHS, 'r') as fin:
+#     for img_path in fin:
+#       if img_count % _NUM_TASKS == _TASK_ID:
+#         img_path = img_path.strip()
+#         img_list.append(img_path)
+#       img_count += 1
+#   return img_list
 
 
-def _get_index(in_data, points):
-  '''
-  Args:
-    in_data: [None, 2]
-    points:
-  '''
-  expand_in_data = in_data[:, np.newaxis, :]
-  distance = np.sum(np.square(expand_in_data - points), axis=2)
-  index = np.argmin(distance, axis=1)
-  return index
+# def _get_index(in_data, points):
+#   '''
+#   Args:
+#     in_data: [None, 2]
+#     points:
+#   '''
+#   expand_in_data = in_data[:, np.newaxis, :]
+#   distance = np.sum(np.square(expand_in_data - points), axis=2)
+#   index = np.argmin(distance, axis=1)
+#   return index
 
 
-def _calculate_prior(img_path, points, probs):
-  img = imread(img_path)
-  img = resize(img, (224, 224), preserve_range=True)
-  if len(img.shape)!=3 or img.shape[2]!=3:
-    return probs
-  img_lab = color.rgb2lab(img)
-  img_lab = img_lab.reshape((-1, 3))
-  img_ab = img_lab[:, 1:]
-  nd_index = _get_index(img_ab, points)
-  for i in nd_index:
-    probs[i] += 1
+# def _calculate_prior(img_path, points, probs):
+#   img = imread(img_path)
+#   img = resize(img, (224, 224), preserve_range=True)
+#   if len(img.shape)!=3 or img.shape[2]!=3:
+#     return probs
+#   img_lab = color.rgb2lab(img)
+#   img_lab = img_lab.reshape((-1, 3))
+#   img_ab = img_lab[:, 1:]
+#   nd_index = _get_index(img_ab, points)
+#   for i in nd_index:
+#     probs[i] += 1
 
 
-def main():
-  points = np.load(_POINTS_PATH)
-  points = points.astype(np.float64)
-  points = points[None, :, :]
+# def main():
+#   points = np.load(_POINTS_PATH)
+#   points = points.astype(np.float64)
+#   points = points[None, :, :]
   
-  img_list = _get_img_list()
-  probs = np.zeros((313), dtype=np.float64)   
-  img_count = 0
-  start_time = monotonic.monotonic()
+#   img_list = _get_img_list()
+#   probs = np.zeros((313), dtype=np.float64)   
+#   img_count = 0
+#   start_time = monotonic.monotonic()
 
-  for img_path in img_list:
-    _calculate_prior(img_path, points, probs)
-    img_count += 1
-    if img_count % _PRINT_FREQ == 0:
-      print(img_count, monotonic.monotonic()-start_time)
-      start_time = monotonic.monotonic()
+#   for img_path in img_list:
+#     _calculate_prior(img_path, points, probs)
+#     img_count += 1
+#     if img_count % _PRINT_FREQ == 0:
+#       print(img_count, monotonic.monotonic()-start_time)
+#       start_time = monotonic.monotonic()
 
-  probs = probs / np.sum(probs)
-  np.save('probs_{}'.format(_TASK_ID), probs)
+#   probs = probs / np.sum(probs)
+#   np.save('probs_{}'.format(_TASK_ID), probs)
 
 
-if __name__ == "__main__":
-  main()
+# if __name__ == "__main__":
+#   main()
