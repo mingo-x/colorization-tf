05472411d9b5c77b839cdd8e8acacb03bec51637
Yanping <rea.s.xyp@gmail.com>
2018-07-31 11:25:59 +0200

    Change prior calculation to multi-process

--- tools/create_prior_probs.py
+++ tools/create_prior_probs.py
@@ -1,51 +1,74 @@
-import tensorflow as tf 
 import numpy as np
 from skimage.io import imread
 from skimage import color
-import sys
-import random
 from skimage.transform import resize
+from multiprocessing import Pool
 
-lists_f = open('data/train.txt')
-points = np.load('resources/pts_in_hull.npy')
-points = points.astype(np.float64)
-points = points[None, :, :]
-filename_lists = []
-probs = np.zeros((313), dtype=np.float64)
-num = 0
 
-for img_f in lists_f:
-  img_f = img_f.strip()
-  filename_lists.append(img_f)
-random.shuffle(filename_lists)
+_NUM_PROCESSES = 130
+_IMG_PATHS = 'data/train.txt'
+_POINTS_PATH = 'resources/pts_in_hull.npy'
+_PRINT_FREQ = 100
 
-#construct graph
-in_data = tf.placeholder(tf.float64, [None, 2])
-expand_in_data = tf.expand_dims(in_data, axis=1)
 
-distance = tf.reduce_sum(tf.square(expand_in_data - points), axis=2)
-index = tf.argmin(distance, axis=1)
-config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
-sess = tf.Session(config=config)
+def _get_img_list():
+  img_list = []  
+  with open(_IMG_PATHS, 'r') as fin:
+    for img_path in fin:
+      img_path = img_path.strip()
+      img_list.append(img_path)
+  return img_list
 
-for img_f in filename_lists:
-  img_f = img_f.strip()
-  img = imread(img_f)
+
+def _get_index(in_data, points):
+  '''
+  Args:
+    in_data: [None, 2]
+    points:
+  '''
+  expand_in_data = in_data[:, np.newaxis, :]
+  distance = np.sum(np.square(expand_in_data - points), axis=2)
+  index = np.argmin(distance, axis=1)
+  return index
+
+
+def _calculate_prior(img_path, points):
+  probs = np.zeros((_NUM_PROCESSES, 313), dtype=np.float64)
+  img = imread(img_path)
   img = resize(img, (224, 224), preserve_range=True)
   if len(img.shape)!=3 or img.shape[2]!=3:
     continue
   img_lab = color.rgb2lab(img)
   img_lab = img_lab.reshape((-1, 3))
   img_ab = img_lab[:, 1:]
-  nd_index = sess.run(index, feed_dict={in_data: img_ab})
+  nd_index = _get_index(img_ab, points)
   for i in nd_index:
-    i = int(i)
     probs[i] += 1
-  print(num)
-  sys.stdout.flush()
-  num += 1
-sess.close()
-probs = probs / np.sum(probs)
+   
+  probs = probs / np.sum(probs)
+  return probs
 
-np.save('probs', probs)
+
+def main():
+  points = np.load(_POINTS_PATH)
+  points = points.astype(np.float64)
+  points = points[None, :, :]
+  
+  img_list = _get_img_list()
+  probs = np.zeros((_NUM_PROCESSES, 313), dtype=np.float64)
+  pool = Pool(processes=_NUM_PROCESSES)   
+  calculate_prior = lambda x: _calculate_prior(x, points)
+  img_count = 0
+
+  for prior in pool.imap_unordered(calculate_prior, img_list):
+    probs += prior
+    img_count += 1
+    if img_count % _PRINT_FREQ == 0:
+      print(img_count)
+
+  probs = probs / np.sum(probs)
+  np.save('probs', probs)
+
+
+if __name__ == "__main__":
+  main()
