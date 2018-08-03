df314e3c76cc4735bc632e8b698801cdc19b7a36
Yanping <rea.s.xyp@gmail.com>
2018-08-03 17:43:10 +0200

    Fix.

--- net.py
+++ net.py
@@ -149,7 +149,6 @@ class Net(object):
         with tf.variable_scope('D', reuse=reuse):
             # data_ab = tf.stop_gradient(data_lab)
             # original_shape = tf.shape(data_ab)
-            
             # Upscale.
             # 176x176
             conv_num = 1
@@ -197,8 +196,8 @@ class Net(object):
         cc = tf.constant(cc, dtype=tf.float32)  # [313, 2]
         # cc = tf.expand_dims(cc, 0) 
         # conv8_313 = conv8_313[0, :, :, :]
-        shape = tf.shape(class8_313_rh)
         conv8_313_rh = conv8_313 * rebalance
+        shape = tf.shape(class8_313_rh)
         class8_313_rh = tf.reshape(class8_313_rh, (-1, 313))  # [N*H*W/16, 313]
         class8_313_rh = tf.nn.softmax(conv8_313_rh, axis=-1)  # [N*H*W/16, 313]
         
