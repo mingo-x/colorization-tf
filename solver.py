a7317e252b0ce68c45c9b002b7add3e7ea519838
Yanping <rea.s.xyp@gmail.com>
2018-08-05 22:22:10 +0200

    Test.

--- solver.py
+++ solver.py
@@ -117,7 +117,10 @@ class Solver(object):
         saver.restore(sess, self.ckpt)
         print(self.ckpt + " restored.")
         start_step = sess.run(self.global_step)
+        start_step = 230000
+        sess.run(self.global_step.assign(start_step))
         print("Global step: {}".format(start_step))
+
       else:
         sess.run(init)
         print("Initialized.")
