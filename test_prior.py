b4ac23e710f44b18af79f5722fb93f11799c7ac0
Yanping <rea.s.xyp@gmail.com>
2018-08-07 16:33:58 +0200

    Test prior.

---
+++ test_prior.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+import utils
+
+_IMG_PATH = '/srv/glusterfs/xieya/data/imagenet1k_uncompressed/val/ILSVRC2012_val_00050000.JPEG'
+img = cv2.imread(_IMG_PATH)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = [img]
+img = np.asarray(img, dtype=np.uint8)
+_, data_ab = utils.preprocess(img, training=False)
+prior = utils.get_prior(data_ab)
+prior = prior[0, :, :, 0]
+e = np.sum(prior) / (prior.shape[0] * prior.shape[1])
