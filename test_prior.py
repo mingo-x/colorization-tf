440a1102afb5b5d7c0a566075ef3910c5d9256e0
Yanping <rea.s.xyp@gmail.com>
2018-08-07 16:53:47 +0200

    Test prior.

--- test_prior.py
+++ test_prior.py
@@ -6,8 +6,8 @@ import utils
 
 _IMG_PATH = '/srv/glusterfs/xieya/data/imagenet1k_uncompressed/val/ILSVRC2012_val_00050000.JPEG'
 img = cv2.imread(_IMG_PATH)
-img = resize(img, (224, 224))
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = resize(img, (224, 224), preserve_range=True)
 img = [img]
 img = np.asarray(img, dtype=np.uint8)
 _, data_ab = utils.preprocess(img, training=False)
