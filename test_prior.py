861cd0a715802fcf9b3ab9cff23ec238ad3f8350
Yanping <rea.s.xyp@gmail.com>
2018-08-07 16:51:46 +0200

    Test prior.

--- test_prior.py
+++ test_prior.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+from skimage.transform import resize
 
 import utils
 
 _IMG_PATH = '/srv/glusterfs/xieya/data/imagenet1k_uncompressed/val/ILSVRC2012_val_00050000.JPEG'
 img = cv2.imread(_IMG_PATH)
+img = resize(img, (224, 224))
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = [img]
 img = np.asarray(img, dtype=np.uint8)
