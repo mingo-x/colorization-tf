176989d2e154ffc7a3ca7a1c357cfcfb5ca76367
Yanping <rea.s.xyp@gmail.com>
2018-08-09 16:42:54 +0200

    Structure gray image.

--- train_data.py
+++ train_data.py
@@ -18,6 +18,7 @@
 #$ -j y
 
 import os
+import subprocess
 import sys
 
 _TASK_ID = os.environ.get('SGE_TASK_ID')
@@ -26,6 +27,7 @@ if _TASK_ID is not None:
     _TASK_ID = int(_TASK_ID) - 1
 
 _COLOR_DIR = '/srv/glusterfs/xieya/data/imagenet_colorized/train'
+_GRAY_TRAIN_DIR = '/srv/glusterfs/xieya/data/imagenet_gray/train'
 _LOG_FREQ = 100
 _TASK_NUM = 100
 _TRAIN_DATA_LIST = 'data/train.txt'
@@ -47,6 +49,25 @@ def count_img():
                     print(img_name)
             line_idx += 1
     return count
+
+def structure_gray():
+    line_idx = 0
+    count = 0
+    with open(_TRAIN_DATA_LIST, 'r') as fin:
+        for line in fin:
+            if line_idx % _TASK_NUM == _TASK_ID:
+                img_path = line.strip()
+                img_name = os.path.split(img_path)
+                class_name = img_name.split('_')[0]
+                class_path = os.path.join(_GRAY_TRAIN_DIR, class_name)
+                subprocess.check_call(['mkdir', '-p', class_path])
+                gray_path = os.path.join(_GRAY_TRAIN_DIR, img_name)
+                subprocess.check_call(['mv', gray_path, class_path + '/'])
+                count += 1
+                if count % _LOG_FREQ == 0:
+                    print(count)
+                    sys.stdout.flush()
+            line_idx += 1
 
 
 if __name__ == "__main__":
