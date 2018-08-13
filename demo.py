fa330eca90b28d8874aa40e65d5f35035b2abaab
xyp <rea.s.xyp@gmail.com>
2018-08-13 13:50:57 +0200

    .

--- demo.py
+++ demo.py
@@ -11,9 +11,9 @@ _RESIZE_SIZE = 0
 _CIFAR_IMG_SIZE = 32
 _CIFAR_BATCH_SIZE = 20
 _CIFAR_COUNT = 0
-_CKPT_PATH = '/srv/glusterfs/xieya/colorization-gan-6/models/model.ckpt-456000'
-IMG_DIR = '/srv/glusterfs/xieya/image/grayscale/colorization_test'
-OUTPUT_DIR = '/srv/glusterfs/xieya/image/color/colorization_test'
+_CKPT_PATH = '/srv/glusterfs/xieya/colorization-gan-6/models/model.ckpt-457000'
+IMG_DIR = '/srv/glusterfs/xieya/image/grayscale'
+OUTPUT_DIR = '/srv/glusterfs/xieya/image/color'
 T = 2.63
 
 
@@ -173,6 +173,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-    # _colorize_high_res_img()
+    # main()
+    _colorize_high_res_img()
     # cifar()
