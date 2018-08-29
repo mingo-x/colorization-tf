e0ec7739dba09096c73da2f8f6f7af5bcedd4172
Yanping <rea.s.xyp@gmail.com>
2018-08-29 20:52:54 +0200

    Fix.

--- net.py
+++ net.py
@@ -201,33 +201,33 @@ class Net(object):
                 
                 discriminator = conv_4
             elif self.version == 3:
-                self.downscale = 16
-                # 64x64x64
+                self.downscale = (44. * 44.) /(3. * 3.)
+                # 44x44x64
                 conv_num = 1
                 conv_1 = conv2d('d_conv_{}'.format(conv_num), data_313, [4, 4, 3, 64], stride=1, relu=False, wd=None, leaky=True)
-                # 32x32x64
+                # 22x22x64
                 conv_num += 1
                 conv_2 = conv2d('d_conv_{}'.format(conv_num), conv_1, [4, 4, 64, 64], stride=2, relu=False, wd=None)
                 bn_1 = batch_norm('bn_1', conv_2, train=self.train)
                 conv_2 = tf.nn.leaky_relu(bn_1)
-                # 16x16x128
+                # 11x11x128
                 conv_num += 1
                 conv_3 = conv2d('d_conv_{}'.format(conv_num), conv_2, [4, 4, 64, 128], stride=2, relu=False, wd=None);
                 bn_2 = batch_norm('bn_2', conv_3, train=self.train)
                 conv_3 = tf.nn.leaky_relu(bn_2)
-                # 8x8x128
+                # 5x5x128
                 conv_num += 1
-                conv_4 = conv2d('d_conv_{}'.format(conv_num), conv_3, [4, 4, 128, 128], stride=2, relu=False, wd=None);
+                conv_4 = conv2d('d_conv_{}'.format(conv_num), conv_3, [3, 3, 128, 128], stride=2, relu=False, wd=None);
                 bn_3 = batch_norm('bn_3', conv_4, train=self.train)
                 conv_4 = tf.nn.leaky_relu(bn_3)
-                # 4x4x256
+                # 3x3x256
                 conv_num += 1
-                conv_5 = conv2d('d_conv_{}'.format(conv_num), conv_4, [4, 4, 128, 256], stride=2, relu=False, wd=None);
+                conv_5 = conv2d('d_conv_{}'.format(conv_num), conv_4, [3, 3, 128, 256], stride=2, relu=False, wd=None);
                 bn_4 = batch_norm('bn_4', conv_5, train=self.train)
                 conv_5 = tf.nn.leaky_relu(bn_4)
-                # 4x4x1
+                # 3x3x1
                 conv_num += 1
-                conv_6 = conv2d('d_conv_{}'.format(conv_num), conv_5, [4, 4, 256, 1], stride=1, relu=False, wd=None, sigmoid=True);
+                conv_6 = conv2d('d_conv_{}'.format(conv_num), conv_5, [3, 3, 256, 1], stride=1, relu=False, wd=None, sigmoid=True);
                 
                 discriminator = conv_6
             else:
