eb2ab9d578d77e61e8fd28a04698ce2eb01a0a72
xyp <rea.s.xyp@gmail.com>
2018-08-29 12:42:59 +0200

    .

--- demo.py
+++ demo.py
@@ -13,12 +13,12 @@ _RESIZE_SIZE = 0
 _CIFAR_IMG_SIZE = 32
 _CIFAR_BATCH_SIZE = 20
 _CIFAR_COUNT = 0
-_CKPT_PATH = '/srv/glusterfs/xieya/colorization-test/models/model.ckpt-90000'
+_CKPT_PATH = '/srv/glusterfs/xieya/colorization-test/models/model.ckpt-66000'
 IMG_DIR = '/srv/glusterfs/xieya/image/grayscale/colorization_test'
 OUTPUT_DIR = '/srv/glusterfs/xieya/image/color/colorization_test'
 _IMG_NAME = '/srv/glusterfs/xieya/image/grayscale/cow_gray.jpg'
-#T = 2.63
-T = 3.0
+T = 2.63
+#T = 3.0
 
 
 def _resize(img, resize_size=0):
