482ffb43a7a8182e30ef3f96100066628cf09903
Yanping <rea.s.xyp@gmail.com>
2018-09-08 19:32:37 +0200

    Visualize wgan_ab.

--- demo.py
+++ demo.py
@@ -176,8 +176,57 @@ def main():
                 _colorize_single_img(img_name, model, input_tensor, sess)
 
 
+def save_images(X, save_path):
+    # [0, 1] -> [0,255]
+    if isinstance(X.flatten()[0], np.floating):
+        X = (255.99*X).astype('uint8')
+
+    n_samples = X.shape[0]
+    rows = int(np.sqrt(n_samples))
+    while n_samples % rows != 0:
+        rows -= 1
+
+    nh, nw = rows, n_samples/rows
+
+    if X.ndim == 2:
+        X = np.reshape(X, (X.shape[0], int(np.sqrt(X.shape[1])), int(np.sqrt(X.shape[1]))))
+
+    if X.ndim == 4:
+        # BCHW -> BHWC
+        # X = X.transpose(0,2,3,1)
+        h, w = X[0].shape[:2]
+        img = np.zeros((h*nh, w*nw, 3))
+    elif X.ndim == 3:
+        h, w = X[0].shape[:2]
+        img = np.zeros((h*nh, w*nw))
+
+    for n, x in enumerate(X):
+        j = n/nw
+        i = n%nw
+        img[j*h:j*h+h, i*w:i*w+w] = x
+
+    imsave(save_path, img)
+
+
+def demo_wgan_ab():
+    noise = tf.constant(np.random.normal(size=(64, 128)).astype('float32'))
+    model = Net(train=False)
+    model.output_dim = 2
+    colorized = model.GAN_G(noise)
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        saver.restore(sess, _CKPT_PATH)
+        ab = session.run(colorized) # [-1, 1]
+        ab *= 110.
+        l = np.full((64, 64, 64, 1), 50)
+        lab = np.concatenate((l, ab), axis=-1)
+        rgb = color.lab2rgb(lab)
+        save_images(rgb, '/srv/glusterfs/xieya/image/color/samples.png')
+
 if __name__ == "__main__":
     subprocess.check_call(['mkdir', '-p', OUTPUT_DIR])
-    main()
+    # main()
+    demo_wgan_ab()
     # _colorize_high_res_img(_IMG_NAME)
     # cifar()
