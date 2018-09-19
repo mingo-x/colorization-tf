87dced1bb4e436a3be42ecfa1957ec1d21ea49b5
Yanping <rea.s.xyp@gmail.com>
2018-09-19 22:43:40 +0200

    Test.

--- utils.py
+++ utils.py
@@ -343,18 +343,21 @@ def decode(data_l, conv8_313, rebalance=1, propagate=False):
         # if prob < mean_norm:
           # data_ab[i, j, :] = 0
         ab = [data_ab[i, j]]
-        wt = [1]
+        wt = [1.]
         for nb in nbs:
           ni = i + nb[0]
           nj = j + nb[1]
           if ni >=0 and ni < height_ss and nj >= 0 and nj < width_ss:
             if norm[ni, nj] > prob:
               ab.append(data_ab[ni, nj])
-              wt.append(1. / abs(data_l[i, j, 0] - data_l[ni, nj, 0]))
+              if data_l[i, j, 0] == data_l[ni, nj, 0]:
+                wt.append(1.)
+              else:
+                wt.append(1. / abs(data_l[i, j, 0] - data_l[ni, nj, 0]))
         ab = np.array(ab)
         wt = np.array(wt)
         ave_ab = np.average(ab, axis=0, weights=wt)
-        print(ave_ab[0], ave_ab[1])
+        # print(ave_ab[0], ave_ab[1])
         new_ab[i, j, 0] = ave_ab[0]
         new_ab[i, j, 1] = ave_ab[1]
         # for nb in nbs:
