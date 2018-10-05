f1d8fccfea771921c28294081e5054d43e7a154d
Yanping <rea.s.xyp@gmail.com>
2018-10-05 12:41:17 +0200

    With language.

--- solver_language.py
+++ solver_language.py
@@ -2,13 +2,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import h5py
 import numpy as np
 from skimage import io, color
 import tensorflow as tf
 
 from ops import *
 from net import Net
-from data import DataSet
+from data_coco import DataSet
 import time
 from datetime import datetime
 import os
@@ -76,6 +77,10 @@ class Solver(object):
         with tf.device('/gpu:' + str(self.device_id)):
             self.data_l = tf.placeholder(
                 tf.float32, (self.batch_size, self.height, self.width, 1))
+            self.captions = tf.placeholder(
+                tf.int32, (self.batch_size, 20))
+            self.lens = tf.placeholder(
+                tf.int32, (self.batch_size))
             self.gt_ab_313 = tf.placeholder(
                 tf.float32,
                 (self.batch_size, int(self.height / 4), int(self.width / 4), 313)
@@ -84,53 +89,38 @@ class Solver(object):
                 tf.float32,
                 (self.batch_size, int(self.height / 4), int(self.width / 4), 1)
             )
-            self.data_l_ss = tf.placeholder(tf.float32, (self.batch_size, int(self.height / 4), int(self.width / 4), 1))
 
-            conv8_313 = self.net.inference(self.data_l)
-            if self.dataset.c313:
-                conv8_313_prob = tf.nn.softmax(conv8_313)
-                self.data_fake = tf.concat([self.data_l_ss, conv8_313_prob], axis=-1)
-                self.data_real = tf.placeholder(tf.float32, (self.batch_size, int(self.height / 4), int(self.width / 4), 314))
-            else:
-                ab_fake_ss = self.net.conv313_to_ab(conv8_313)
-                self.data_fake = tf.concat([self.data_l_ss, ab_fake_ss / 110.], axis=-1)
-                self.data_real = tf.placeholder(tf.float32, (self.batch_size, int(self.height / 4), int(self.width / 4), 3))
-                # self.test_real = tf.concat([(self.data_real[0, :, :, 0:1] + 1) * 50, self.data_real[0, :, :, 1:] * 110.], axis=-1)
-                # self.test_fake = tf.concat([(self.data_l_ss [0, :, :, :]+ 1.) * 50., ab_fake_ss[0, :, :, :]], axis=-1)
+            conv8_313 = self.net.inference4(self.data_l, self.captions, self.lens)
 
-
-            new_loss, g_loss, adv_loss = self.net.loss(
+            new_loss, g_loss, _ = self.net.loss(
                 scope, conv8_313, self.prior_boost_nongray,
                 self.gt_ab_313, self.data_fake, self.gan,
                 self.prior_boost)
             tf.summary.scalar('new_loss', new_loss)
             tf.summary.scalar('total_loss', g_loss)
 
-            if self.gan:
-                D_loss, real_score, fake_score, mixed_norm = self.net.discriminator_loss(self.data_real, self.data_fake)
-                tf.summary.scalar('D loss', D_loss)
-                tf.summary.scalar('real_score', real_score)
-                tf.summary.scalar('fake_score', fake_score)
-                tf.summary.scalar('distance', real_score - fake_score)
-                tf.summary.scalar('mixed_norm', mixed_norm)
-                return (new_loss, g_loss, adv_loss, D_loss, real_score, fake_score, mixed_norm)
-            else:
-                return (new_loss, g_loss, adv_loss, None, None, None, None)
+            return new_loss, g_loss
 
 
     def train_model(self):
+        # Load data
+        hfile = '/srv/glusterfs/xieya/data/coco_colors.h5'
+        hf = h5py.File(hfile, 'r')
+        train_origs = hf['train_ims']            
+        train_words = hf['train_words']                                         
+        train_lengths = hf['train_length'] 
+
         with tf.device('/gpu:' + str(self.device_id)):
             self.global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
             learning_rate = tf.train.exponential_decay(self.learning_rate, self.global_step,
                 self.decay_steps, self.lr_decay, staircase=True)
 
             train_vocab = pickle.load(open('./priors/coco_colors_vocab.p', 'r'))
-            train_vocab_embeddings = pickle.load(open('./priors/w2v_embeddings_colors.p', 'r'))
             vrev = dict((v,k) for (k,v) in train_vocab.iteritems())          
             n_vocab = len(train_vocab)
 
             with tf.name_scope('gpu') as scope:
-                self.new_loss, self.total_loss, self.adv_loss, self.D_loss, self.real_score, self.fake_score, self.mixed_norm = self.construct_graph(scope)
+                self.new_loss, self.total_loss = self.construct_graph(scope)
                 self.summaries = tf.get_collection(
                     tf.GraphKeys.SUMMARIES, scope)
 
@@ -140,20 +130,8 @@ class Solver(object):
             opt = tf.train.AdamOptimizer(
                 learning_rate=learning_rate, beta1=self.moment, beta2=0.9)
             G_vars = tf.trainable_variables(scope='G')
-            T_vars = tf.trainable_variables(scope='T')
 
-            with tf.variable_scope('T', reuse=True):
-                if not self.dataset.c313:
-                    T = tf.get_variable('T')
-                    T_loss = tf.multiply(tf.nn.l2_loss(T - 2.63), 1e-3, name='weight_loss')
-                    self.summaries.append(tf.summary.scalar(T.op.name, T[0]))
-                else:
-                    T_loss = 0.
-
-            if self.gan:
-                grads = opt.compute_gradients(self.new_loss * self.net.alpha + self.adv_loss + T_loss, var_list=G_vars + T_vars)
-            else:
-                grads = opt.compute_gradients(self.new_loss, var_list=G_vars)
+            grads = opt.compute_gradients(self.new_loss, var_list=G_vars)
 
             apply_gradient_op = opt.apply_gradients(
                 grads, global_step=self.global_step)
@@ -163,15 +141,6 @@ class Solver(object):
                 0.999, self.global_step)
             variables_averages_op = variable_averages.apply(G_vars + T_vars)
             train_op = tf.group(apply_gradient_op, variables_averages_op)
-
-            if self.gan:
-                D_opt = tf.train.AdamOptimizer(
-                    learning_rate=D_learning_rate, beta1=self.moment, beta2=0.9)
-                D_vars = tf.trainable_variables(scope='D')
-                # for var in D_vars:
-                #     self.summaries.append(tf.summary.histogram(var.op.name, var))
-                D_grads = D_opt.compute_gradients(self.D_loss, var_list=D_vars)
-                D_apply_gradient_op = D_opt.apply_gradients(D_grads)
 
             saver = tf.train.Saver(write_version=tf.train.SaverDef.V2)
             summary_op = tf.summary.merge(self.summaries)
@@ -205,35 +174,17 @@ class Solver(object):
                     init_saver.restore(sess, self.init_ckpt)
                     print('Init generator with {}.'.format(self.init_ckpt))
 
-            if not self.dataset.c313:
-                start_temp = sess.run(T)
-                print("Temp {}.".format(start_temp))
-
             summary_writer = tf.summary.FileWriter(self.train_dir, sess.graph)
             start_time = time.time()
 
             start_step = int(start_step)
             for step in xrange(start_step, self.max_steps, self.g_repeat):
-                if self.gan:
-                    for _ in xrange(self.d_repeat):
-                        data_l, _, _, data_real = self.dataset.batch()
-                        data_l_ss = data_real[:, :, :, 0: 1]
-                        if not self.corr:
-                            _, _, _, data_real = self.dataset.batch()
-                        # Discriminator training.
-                        sess.run([D_apply_gradient_op],
-                                  feed_dict={self.data_l: data_l, self.data_real: data_real, self.data_l_ss: data_l_ss})
-
-                    if step % _LOG_FREQ < self.g_repeat:
-                        d_loss_value, real_score_value, fake_score_value = sess.run([self.D_loss, self.real_score, self.fake_score], 
-                            feed_dict={self.data_l:data_l, self.data_real: data_real, self.data_l_ss: data_l_ss})
-
                 # Generator training.
                 for _ in xrange(self.g_repeat):
-                    data_l, gt_ab_313, prior_boost_nongray, data_real = self.dataset.batch()
-                    data_l_ss = data_real[:, :, :, 0: 1]
+                    data_l, gt_ab_313, prior_boost_nongray, captions, lens = self.dataset.batch()
                     sess.run([train_op], 
-                              feed_dict={self.data_l:data_l, self.gt_ab_313:gt_ab_313, self.prior_boost_nongray:prior_boost_nongray, self.data_l_ss: data_l_ss})
+                              feed_dict={self.data_l:data_l, self.gt_ab_313:gt_ab_313, self.prior_boost_nongray:prior_boost_nongray,
+                              self.captions:caption, self.lens:lens})
 
                 if step % _LOG_FREQ < self.g_repeat:
                     duration = time.time() - start_time
@@ -241,48 +192,19 @@ class Solver(object):
                     examples_per_sec = num_examples_per_step / duration
                     sec_per_batch = duration / (self.num_gpus * _LOG_FREQ)
 
-                    if self.gan:
-                        loss_value, new_loss_value, adv_loss_value= sess.run(
-                          [self.total_loss, self.new_loss, self.adv_loss], 
-                          feed_dict={self.data_l:data_l, self.gt_ab_313:gt_ab_313, self.prior_boost_nongray:prior_boost_nongray, self.data_l_ss: data_l_ss})
-                        format_str = ('%s: step %d, G loss = %.2f, new loss = %.2f, adv = %0.5f, D = %0.5f, div = %0.3f(%.1f examples/sec; %.3f '
-                                      'sec/batch)')
-                        # assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-                        # assert not np.isnan(adv_loss_value), 'Adversarial diverged with loss = NaN'
-                        # assert not np.isnan(D_loss_value), 'Discriminator diverged with loss = NaN'
-                        print (format_str % (datetime.now(), step, loss_value, new_loss_value, real_score_value + adv_loss_value, d_loss_value, real_score_value - fake_score_value,
-                                             examples_per_sec, sec_per_batch))
-                        # print(np.min(data_fake[:, :, :, 0]), np.max(data_fake[:, :, :, 0]), np.min(data_fake[:, :, :, 1]), np.max(data_fake[:, :, :, 2]))
-                        # print(np.min(data_real[:, :, :, 0]), np.max(data_real[:, :, :, 0]), np.min(data_real[:, :, :, 1]), np.max(data_real[:, :, :, 2]))
-                        # if step % 100 == 0:
-                        #     img_lab = np.array(test_real, dtype=np.float64)
-                        #     data_rgb = color.lab2rgb(img_lab)
-                        #     io.imsave(os.path.join(self.train_dir, "{}_real.jpg".format(step)), data_rgb)
-                        #     img_lab = np.array(test_fake, dtype=np.float64)
-                        #     data_rgb = color.lab2rgb(img_lab)
-                        #     io.imsave(os.path.join(self.train_dir, "{}_fake.jpg".format(step)), data_rgb)
-
-                    else:
-                        loss_value, new_loss_value = sess.run(
-                            [self.total_loss, self.new_loss],
-                            feed_dict={self.data_l: data_l, self.gt_ab_313: gt_ab_313, self.prior_boost_nongray: prior_boost_nongray})
-                        format_str = ('%s: step %d, G loss = %.2f, new loss = %.2f(%.1f examples/sec; %.3f '
-                                      'sec/batch)')
-                        # assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-                        # assert not np.isnan(adv_loss_value), 'Adversarial diverged with loss = NaN'
-                        # assert not np.isnan(D_loss_value), 'Discriminator diverged with loss = NaN'
-                        print (format_str % (datetime.now(),
-                                             step, loss_value, new_loss_value,
-                                             examples_per_sec, sec_per_batch))
+                    loss_value, new_loss_value = sess.run(
+                        [self.total_loss, self.new_loss],
+                        feed_dict={self.data_l: data_l, self.gt_ab_313: gt_ab_313, self.prior_boost_nongray: prior_boost_nongray})
+                    format_str = ('%s: step %d, G loss = %.2f, new loss = %.2f(%.1f examples/sec; %.3f '
+                                  'sec/batch)')
+                    print (format_str % (datetime.now(),
+                                         step, loss_value, new_loss_value,
+                                         examples_per_sec, sec_per_batch))
                     start_time = time.time()
 
                 if step % 100 < self.g_repeat:
-                    if self.gan:
-                        summary_str = sess.run(summary_op, feed_dict={self.data_l:data_l, self.gt_ab_313:gt_ab_313, self.prior_boost_nongray:prior_boost_nongray, 
-                            self.data_real: data_real, self.data_l_ss: data_l_ss})
-                    else:
-                        summary_str = sess.run(summary_op, feed_dict={
-                            self.data_l: data_l, self.gt_ab_313: gt_ab_313, self.prior_boost_nongray: prior_boost_nongray})
+                    summary_str = sess.run(summary_op, feed_dict={
+                        self.data_l: data_l, self.gt_ab_313: gt_ab_313, self.prior_boost_nongray: prior_boost_nongray})
                     summary_writer.add_summary(summary_str, step)
 
                 # Save the model checkpoint periodically.
