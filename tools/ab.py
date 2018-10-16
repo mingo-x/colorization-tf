556ea19b39ae186a8d26004e6271d57b75a7fbaf
Yanping <rea.s.xyp@gmail.com>
2018-10-16 21:49:20 +0200

    fix.

--- tools/ab.py
+++ tools/ab.py
@@ -13,8 +13,8 @@ def draw_ab_space_given_l(l):
         for j in xrange(23):
             a = (i - 11) * 10
             b = (j - 11) * 10
-            np[i * cell_size: (i + 1) * cell_size, j * cell_size: (j + 1) * cell_size, 1].fill(a)
-            np[i * cell_size: (i + 1) * cell_size, j * cell_size: (j + 1) * cell_size, 1].fill(b)
+            canvas[i * cell_size: (i + 1) * cell_size, j * cell_size: (j + 1) * cell_size, 1].fill(a)
+            canvas[i * cell_size: (i + 1) * cell_size, j * cell_size: (j + 1) * cell_size, 1].fill(b)
 
     canvas = color.lab2rgb(canvas)
     io.imsave(os.path.join(_OUTPUT_DIR, '{}.jpg'.format(l)), canvas)
@@ -23,6 +23,7 @@ def draw_ab_space_given_l(l):
 def draw_ab_space():
     if not os.path.exists(_OUTPUT_DIR):
         os.makedirs(_OUTPUT_DIR)
+        print('Created dir {}.'.format(_OUTPUT_DIR))
 
     for l in xrange(0, 101, 10):
         draw_ab_space_given_l(l)
