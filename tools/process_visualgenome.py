91b7e1726be497e9f4c0a3f2e106d9561bab6a24
Yanping <rea.s.xyp@gmail.com>
2018-11-01 23:01:02 +0100

    .

--- tools/process_visualgenome.py
+++ tools/process_visualgenome.py
@@ -119,9 +119,9 @@ def scale_regions(region_file_name):
             ny = _scale_to_int(y, scale)
             nw = _scale_to_int(w, scale)
             nh = _scale_to_int(h, scale)
+            print(nx, ny, nw, nh, original_224.shape)
             region_224 = original_224[ny: ny + nh, nx: nx + nw]
             io.imsave('/srv/glusterfs/xieya/tmp/{0}_{1}_224.jpg'.format(img_id, reg_id), region_224)
-            print(nx, ny, nw, nh)
             new_reg = {'region_id': reg['region_id'], 'x': nx, 'y': ny, 'width': nw, 'height': nh, 'phrase': reg['phrase']}
             new_regions.append(new_reg)
 
