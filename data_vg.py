95c5ac080023dbf58d4bc5185cb0ce98a4f72280
Yanping <rea.s.xyp@gmail.com>
2018-11-04 13:14:33 +0100

    Fix.

--- data_vg.py
+++ data_vg.py
@@ -40,6 +40,8 @@ class DataSet(object):
             self.thread_num2 = int(int(dataset_params['thread_num']) / 2)
             self.c313 = True if dataset_params['c313'] == '1' else False
 
+        self.train = train
+
         if train:
             self.split_file = '/srv/glusterfs/xieya/data/visual_genome/train.txt'
         else:
